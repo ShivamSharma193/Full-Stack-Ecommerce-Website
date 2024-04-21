a68464fc23de96b4281247a7dbaa1047782523ba
ShivamSharma193 <sharma.shivam.18in1037@gmail.com>
2024-04-21 23:35:26 +0530

    random

--- Backend/fashion-point/src/test/java/com/example/demo/FashionPointApplicationTests.java
+++ Backend/fashion-point/src/test/java/com/example/demo/FashionPointApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.spr.context.SpringBootTest;
 
 @SpringBootTest
 class FashionPointApplicationTests {
