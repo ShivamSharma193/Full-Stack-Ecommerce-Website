2f1d415f200d2ebdc16d9c4aa34d620c95d01223
unknown <Omkar8446>
2024-04-22 00:33:34 +0530

    mail aaya kya re

--- Backend/fashion-point/src/main/java/com/fashion_point/Controller/AdminOrderController.java
+++ Backend/fashion-point/src/main/java/com/fashion_point/Controller/AdminOrderController.java
@@ -7,49 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fashion_point.Exception.OrderException;
-import com.fashion_point.pojos.OrderD;
-import com.fashion_point.service.OrderService;
-
-@RestController
-@RequestMapping("/api/admin/orders")
-public class AdminOrderController {
-	
-	@Autowired
-	private OrderService orderService;
-	
-	@GetMapping("/")
-	public ResponseEntity<List<OrderD>>getAllOrdersHandler(){
-		List<OrderD>orders=orderService.getAllOrders();
-		return new ResponseEntity<List<OrderD>>(orders,HttpStatus.ACCEPTED);
-	}
-	
-	//to update orrder status ie: to confirm order
-	@PutMapping("/{OrderID}/confirm")
-	public ResponseEntity<OrderD>ConfirmOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String JWT)
-	throws OrderException{
-		OrderD order=orderService.confirmedOrder(orderId);
-		
-		return new ResponseEntity<OrderD>(order,HttpStatus.OK);
-	}
-	
-	@PutMapping("/{OrderID}/ship")
-	public ResponseEntity<OrderD>ShipOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String JWT)
-	throws OrderException{
-		OrderD order=orderService.shippedOrder(orderId);
-		
-		return new ResponseEntity<OrderD>(order,HttpStatus.OK);
-	}
-	
-	@PutMapping("/{OrderID}/deliver")
-	public ResponseEntity<OrderD>deliverOrderHandler(@PathVariable Long orderId,@RequestHeader("Authorization") String JWT)
-	throws OrderException{
+i
 		OrderD order=orderService.deliveredOrder(orderId);
 		
 		return new ResponseEntity<OrderD>(order,HttpStatus.OK);
@@ -73,5 +31,30 @@ public class AdminOrderController {
 	}
 	
 	
+//gonohne
+g
+fghgtryg
+fdsfg
+yuf
+ry
+kjf
+gk
+gfgk
+erqw
+,k
+yr3r
+kyu
+tre
+y
+re
+,jfhr
+e
+m
+55,
 
+kf
+4
+, 
+
+ 
 }
